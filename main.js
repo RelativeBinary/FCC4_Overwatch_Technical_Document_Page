ce52c2ad53c64f5ab937ff60950365c3a5e8bac0
Patrick Bornay <patrick.bornay@protonmail.com>
2018-12-29 14:54:24 +1100

    Rough draft on how the AJAX Component of this website will be handled

---
+++ main.js
@@ -0,0 +1,5 @@
+//for each role section the lists will be generated based from objects with ajax
+//there will be role object > role-offensive > role-defensive > role-etc > role-heros >role-heros-heroname
+//bunch of switch statements which will allocate information e.g
+//if role = tanks then for tanks.offensive do x, for tanks.defensive do x, for tanks.etc do x
+// for tanks.heros.length() get tanks.heros[i] = currHero for currHero do x
