2eb554bcef9202c2a5efe54ee92d9ed2eebd0351
Patrick Bornay <patrick.bornay@protonmail.com>
2018-12-30 23:36:19 +1100

    added dropdown features for the side nav bar

--- main.js
+++ main.js
@@ -3,3 +3,34 @@
 //bunch of switch statements which will allocate information e.g
 //if role = tanks then for tanks.offensive do x, for tanks.defensive do x, for tanks.etc do x
 // for tanks.heros.length() get tanks.heros[i] = currHero for currHero do x
+
+/* list-item */
+function supDrop(value) {
+    console.log(value);
+    switch (value) {
+        case 0:
+            var drop = document.getElementById("tank-drop");
+            if ((drop.style.display == "list-item")) {
+                drop.style.display = "none";
+            } else {
+                drop.style.display = "list-item";
+            }
+            break;
+        case 1:
+            var drop = document.getElementById("dps-drop");
+            if ((drop.style.display == "list-item")) {
+                drop.style.display = "none";
+            } else {
+                drop.style.display = "list-item";
+            }
+            break;
+        case 2:
+            var drop = document.getElementById("sup-drop");
+            if ((drop.style.display == "list-item")) {
+                drop.style.display = "none";
+            } else {
+                drop.style.display = "list-item";
+            }
+            break;
+    }
+}
